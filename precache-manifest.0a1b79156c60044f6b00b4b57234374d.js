3e7648af4b62d673a5814b4d45a3d20d0abc212b
Vishrut Tiwari <vishrut811@gmail.com>
2021-07-12 13:44:18 +0530

    Updates

---
+++ precache-manifest.0a1b79156c60044f6b00b4b57234374d.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcc9daa08bdb0090771176a6d643f70f",
+    "url": "/DiviUp/index.html"
+  },
+  {
+    "revision": "96fbb28ddc66a50039be",
+    "url": "/DiviUp/static/css/main.32e365c7.chunk.css"
+  },
+  {
+    "revision": "87187b8c4985d2ee0f32",
+    "url": "/DiviUp/static/js/2.bf81fedf.chunk.js"
+  },
+  {
+    "revision": "6dd452137ebdafd136b069e62781a19d",
+    "url": "/DiviUp/static/js/2.bf81fedf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96fbb28ddc66a50039be",
+    "url": "/DiviUp/static/js/main.7a6bd07e.chunk.js"
+  },
+  {
+    "revision": "678afe43f1ec37f6a32a",
+    "url": "/DiviUp/static/js/runtime-main.ad4cc935.js"
+  },
+  {
+    "revision": "ac3c1298def54faa25ba494490ed4f4c",
+    "url": "/DiviUp/static/media/NeutrifPro-Bold.ac3c1298.otf"
+  },
+  {
+    "revision": "76f0c14e70c41e5314700073e6919b00",
+    "url": "/DiviUp/static/media/NeutrifPro-Light.76f0c14e.otf"
+  },
+  {
+    "revision": "ea2339315a9d52cb3e7f51328fa10af0",
+    "url": "/DiviUp/static/media/NeutrifPro-Medium.ea233931.otf"
+  },
+  {
+    "revision": "52b0d8c3708d691c44d5156a9e5f2d6b",
+    "url": "/DiviUp/static/media/NeutrifPro-Regular.52b0d8c3.otf"
+  },
+  {
+    "revision": "d5e437924301262fcae652f8c010086f",
+    "url": "/DiviUp/static/media/chart-line-down.d5e43792.svg"
+  },
+  {
+    "revision": "3228d26428457a38a1f31063668593e2",
+    "url": "/DiviUp/static/media/chart-line.3228d264.svg"
+  },
+  {
+    "revision": "6db61f5f7bcc4b5e150439f3f745c6e8",
+    "url": "/DiviUp/static/media/chart-pie-alt.6db61f5f.svg"
+  },
+  {
+    "revision": "b79f7d72a4a12acad952bdef728d3053",
+    "url": "/DiviUp/static/media/chevron-down.b79f7d72.svg"
+  },
+  {
+    "revision": "c2d439afe62dfcd47b72275fa27597cb",
+    "url": "/DiviUp/static/media/coins.c2d439af.svg"
+  },
+  {
+    "revision": "33b4607528e079faff1e1506e9e5eee9",
+    "url": "/DiviUp/static/media/flask.33b46075.svg"
+  },
+  {
+    "revision": "1d7ac05c60b04c0be76d809a0a84faf4",
+    "url": "/DiviUp/static/media/globe.1d7ac05c.svg"
+  },
+  {
+    "revision": "acd3dbbf366fc5d3971de9fe7193d0b7",
+    "url": "/DiviUp/static/media/piggy-bank.acd3dbbf.svg"
+  },
+  {
+    "revision": "a78611684cf0658c2bccd0fbc4ffb4c4",
+    "url": "/DiviUp/static/media/star.a7861168.svg"
+  }
+]);
